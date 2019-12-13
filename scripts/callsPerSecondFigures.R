b6af28811a9ac6f1239ca4b6f44b45652297a5b9
Lukas Hübner <huebnerlukas@gmail.com>
2019-12-13 11:11:44 +0100

    Add callsPerSecond plot for different MPI-Implementations

--- scripts/callsPerSecondFigures.R
+++ scripts/callsPerSecondFigures.R
@@ -1,10 +1,14 @@
 library(readr)
 library(ggplot2)
 library(grid)
+library(dplyr)
+library(stringr)
+library(purrr)
 
-
-datasetLevels =  c("dna_ShiD9_20@1", "dna_rokasD4_20@8", "dna_rokasD6_20@10", "dna_rokasD1_20@20",
+datasetLevels = c("dna_ShiD9_20@1", "dna_rokasD4_20@8", "dna_rokasD6_20@10", "dna_rokasD1_20@20",
                    "dna_rokasD1_20@18", "dna_rokasD1_20@4", "aa_rokasA4_20@8", "dna_rokasD2a_20@1")
+
+mpiLevels = c("impi", "openmpi4.0", "openmpi3.1", "mvapich2")
 
 ### Data ;-) ###
 dataDistribution <- tribble(
@@ -94,3 +98,59 @@ ggplot(
   theme_bw()
 
 cor.test(1 / callsAndWeight$averageCallsPerSecond, callsAndWeight$weightPerThread, method = "pearson" )
+
+# Calls per second different MPI Implementations
+csvDirDifferentMPI <- "/home/lukas/Documents/Uni/Masterarbeit/profiling/differentMpiIImplementations"
+
+# load data from single csv file and add `dataset` column
+readFile <- function(flnm) {
+  read_csv(flnm) %>%
+    mutate(dataset = str_extract(basename(flnm), "(dna|aa)_.+_[:digit:]{1,2}@[:digit:]{1,2}")) %>%
+    mutate(mpi = str_extract(basename(flnm), "^([:alnum:]|\\.)+"))
+}
+
+# Load all data from multiple runs and aggregate it into `data`
+callsPerSecondData_dMPI <-
+  list.files(
+    path = csvDirDifferentMPI,  
+    pattern = "*.callsPerSecond.csv", 
+    full.names = T) %>% 
+  map_df(~readFile(.)) %>%
+  as_tibble() %>%
+  mutate(timer = factor(timer)) %>%
+  mutate(dataset = factor(dataset, levels = datasetLevels)) %>%
+  mutate(mpi = factor(mpi, levels = mpiLevels))
+
+callsPerSecondData_dMPI$dataset <-
+  factor(
+    map_chr(callsPerSecondData_dMPI$dataset, dataset2Label),
+    levels = dataset2Label(datasetLevels))
+
+totalRuntime <- callsPerSecondData_dMPI %>%
+  group_by(timer, dataset) %>%
+  summarise(runtime = max(secondsPassed))
+
+callsPerSecondData_dMPI <- inner_join(callsPerSecondData_dMPI, totalRuntime, by = c("timer", "dataset"))
+
+callsPerSecondData_dMPI <- callsPerSecondData_dMPI %>%
+  mutate(fractionOfRuntime = secondsPassed / runtime)
+
+callsPerSecondData_dMPI %>%
+  filter(secondsPassed > 1) %>%
+  filter(dataset == "DNA dataset rokasD1\n20 nodes with 20 ranks each") %>%
+  ggplot(aes(
+    x = fractionOfRuntime,
+    y = callsPerSecond,
+    color = mpi,
+    linetype = dataset
+  )) +
+  #geom_point(size=0.7) +
+  geom_line() + 
+  theme_bw() +
+  theme(legend.key.height = unit(1.5, "line")) +
+  scale_color_brewer(palette = "Dark2") +
+  labs(
+    x = "fraction of runtime for this task",
+    y = "number of MPI_Allreduce calls per second",
+    colour = "MPI implementation"
+  )
